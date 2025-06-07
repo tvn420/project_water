e90633d0ee433c4b6a93f3a1d33374a422c9f431
Tuong-Vi nguyen <tvn00100@gmail.com>
2025-06-07 14:36:41 +0200

    first push

---
+++ models/raw_data/raw_res_eau_potable.sql
@@ -0,0 +1,14 @@
+select 
+
+cond_id,
+cond_classe_precision,
+COALESCE(cond_diametre,'0') AS cond_diametre,
+cond_type,
+cond_enrobage,
+cond_materiau,
+coordinates,
+geojson,
+COALESCE(cond_annee_pose,1900) AS cond_annee_pose
+
+from 
+{{ source("eau_potable","reseau_deau_potable_conduite") }}
